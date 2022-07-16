7375baa3a0f9421a0f5271491286ceb3ea9aed0e
Darin <dngau94@gmail.com>
2022-07-16 14:16:07 -0400

    added frontend files and folders

---
+++ client/components/Map.jsx
@@ -0,0 +1,24 @@
+/**
+ * ************************************
+ *
+ * @module  Map
+ * @author  Darin Ngau
+ * @date    2022-07-16
+ * @description presentation component that renders the map.
+ * ************************************
+ */
+
+ import React from 'react';
+
+ const Map = props => {
+
+  // this is a placeholder for the map component to be developed later
+
+   return (
+     <div>
+      <a><img src='..images/map.png'></img></a>
+     </div>
+   );
+ };
+ 
+ export default Map;
